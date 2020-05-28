be78ebb06b1a6f2b74c6339b12674c633daa3e13
M.Uzair <uzair.iu17@gmail.com>
2020-05-29 02:28:41 +0500

    added error page and more styling

--- src/App.js
+++ src/App.js
@@ -68,9 +68,32 @@ import youtube from "./apis/youtube";
 export default () => {
   const [videos, setVideos] = useState([]);
   const [selectedVideo, setSelectedVideo] = useState(null);
+  const [errorMessage,setErrorMessage] = useState(null)
+
+  React.useEffect(()=>{
+    handleSubmit('JavaScript')
+  })
 
 
   async function handleSubmit(searchTerm) {
+    try{
+      const { data: { items: videos } } = await youtube.get("search", {
+        params: {
+          part: "snippet",
+          maxResults: 25,
+          key: "AIzaSyDM7nu75sZgnXDnuHoZXKq2ZD2KdzGeqUc",
+          q: searchTerm,
+        }
+      });
+      console.log(videos)
+      setVideos(videos);
+      setSelectedVideo(videos[0]);
+    }catch(error) {
+      let message = error.response.data.error.message.split('.')
+      setErrorMessage(message)
+      console.log(error.response.data.error.message)
+
+    }
     const { data: { items: videos } } = await youtube.get("search", {
       params: {
         part: "snippet",
@@ -97,8 +120,7 @@ export default () => {
             <SearchBar onSubmit={handleSubmit} />
           </Grid>
           <Grid item xs={8}>
-            {/* <Typography>{selectedVideo} </Typography> */}
-            <VideoDetail video={selectedVideo} />
+            <VideoDetail errorMessasge={errorMessage} video={selectedVideo} />
           </Grid>
           <Grid item xs={4}>
             <VideoList videos={videos} onVideoSelect={handleVideoSelect} />
--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -1,20 +1,50 @@
 import React from 'react'
-import { Typography } from "@material-ui/core";
+import { Typography, Grid } from "@material-ui/core";
+import { makeStyles } from '@material-ui/core/styles';
+import { Alert, AlertTitle } from '@material-ui/lab';
 
 
 
-const VideoDetail = ({ video }) => {
-    if (!video) {
-        return <div> Loading..</div>
+const useStyles = makeStyles((theme) => ({
+    root: {
+        width: '100%',
+        '& > * + *': {
+            marginTop: theme.spacing(2),
+        },
+    },
+}));
+
+const VideoDetail = ({ video, errorMessasge }) => {
+    const classes = useStyles();
+    console.log(errorMessasge,'details')
+    if (!video ) {
+        return (<div className={classes.root}>
+            <Alert severity="error">
+                <AlertTitle>Error</AlertTitle>
+         {errorMessasge} <strong>check it out!</strong>
+            </Alert>
+        </div>
+        )
     }
+    const pusblishTime = video.snippet.publishTime.substr(0, 10)
+
     return (
 
         <div>
             <iframe width="800" height="400" src={`https://www.youtube.com/embed/${video.id.videoId}`} frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-            <Typography style={{padding:'5px',fontSize:'25px'}}> {video.snippet.title} </Typography>
-            <Typography> {video.snippet.description} </Typography>
-            <Typography> {video.snippet.channelTitle} </Typography>
-            <Typography> {video.snippet.publishTime} </Typography>
+            <Typography style={{ padding: '5px', fontSize: '25px' }}> {video.snippet.title} </Typography>
+            <Grid item style={{ marginBottom: '10px', border: '2px' }}>
+                <Typography> Description: </Typography>
+
+                <Typography> {video.snippet.description} </Typography>
+            </Grid>
+            <Grid style={{ marginBottom: '5px' }}>
+                <Typography> Channel: {video.snippet.channelTitle} </Typography>
+            </Grid>
+            <Grid>
+                <Typography> Published on: {pusblishTime} </Typography>
+            </Grid>
+
         </div>
 
 
